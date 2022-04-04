fa97d381b167bfd8947a76b160da04eaa7753dda
P1 <you@example.com>
2022-04-04 09:57:43 -0700

    h

--- corgis.py
+++ corgis.py
@@ -1,20 +1,25 @@
 from flask import Flask, url_for, render_template, request
-import random
+import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/")
+@app.route("/page1.html")
 def render_main():
-    return render_template('home.html')
+    return render_template('page1.html')
 
 @app.route("/response")
 def render_response():
-    json = requests.get('video_games.json')
-    one = request.args['one']
-    two = request.args['two']
-    three = request.args['three']
-    four = request.args['four']
-    yes = request.args['yes']
-    no = request.args['no']
-    nopref = request.args['nopref']
+    playercount = request.args['playercount']
+    handheld = request.args['handheld']
     
+
+    if handheld == yes and playercount == 1:
+        response = hello
+    else:
+        response = goodbye
+    return render_template('response.html', response = response)
+
+        
+        
+if __name__=="__main__":
+    app.run(debug=False)
