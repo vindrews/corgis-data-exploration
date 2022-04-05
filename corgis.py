938f49c92e9648d4a7250f32888415b11b9348c0
P1 <you@example.com>
2022-04-05 09:55:09 -0700

    bwahh

--- corgis.py
+++ corgis.py
@@ -1,25 +1,23 @@
 from flask import Flask, url_for, render_template, request
 import json
 
-app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app = Flask(__name__) 
 
 @app.route("/page1.html")
 def render_main():
     return render_template('page1.html')
 
-@app.route("/response")
+@app.route("/response.html")
 def render_response():
+    with open ('video_games.json') as ratings_data:
+        data = json.load(ratings_data)
     playercount = request.args['playercount']
     handheld = request.args['handheld']
-    
-
-    if handheld == yes and playercount == 1:
-        response = hello
-    else:
-        response = goodbye
-    return render_template('response.html', response = response)
-
-        
-        
+    rating = 0
+    game = ['Title']
+    for x in data:
+        if x['Max Players'] == playercount and x['Handheld?'] == handheld and s['Review Score'] > rating:
+            rating = s['Review Score']
+    return render_template('response.html', game = game)
 if __name__=="__main__":
     app.run(debug=False)
