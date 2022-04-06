0c3a5d11641dff329f288c83ef3a8abb0816be9f
P1 <you@example.com>
2022-04-06 09:55:56 -0700

    wahoo

--- corgis.py
+++ corgis.py
@@ -3,11 +3,15 @@ import json
 
 app = Flask(__name__) 
 
-@app.route("/page1.html")
-def render_main():
+@app.route("/")
+def render_home():
+    return render_template('home.html')
+
+@app.route("/page1")
+def render_page1():
     return render_template('page1.html')
 
-@app.route("/response.html")
+@app.route("/response")
 def render_response():
     with open ('video_games.json') as ratings_data:
         data = json.load(ratings_data)
@@ -18,6 +22,8 @@ def render_response():
     for x in data:
         if x['Max Players'] == playercount and x['Handheld?'] == handheld and s['Review Score'] > rating:
             rating = s['Review Score']
+            handheld = s['handheld']
+            playercount = s['playercount']
     return render_template('response.html', game = game)
 if __name__=="__main__":
     app.run(debug=False)
