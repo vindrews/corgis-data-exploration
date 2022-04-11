199c618fa561ca3d2372bc03b3b03af3e8be98b5
P1 <you@example.com>
2022-04-11 09:47:32 -0700

    !!!!! working

--- corgis.py
+++ corgis.py
@@ -13,19 +13,23 @@ def render_page1():
     if "playercount" in request.args:
         with open ('video_games.json') as ratings_data:
             data = json.load(ratings_data)
-        playercount = request.args['playercount']
-        handheld = request.args['handheld']
+        playercount = int(request.args['playercount'])
+        year = request.args['year']
         rating = 0
-        game = ['Title']
         output = ""
         for x in data:
-            if x['Features']['Max Players'] == playercount and x['Features']['Handheld?'] == handheld and s['Review Score'] > rating:
-                rating = s['Metrics']['Review Score']
-                handheld = s['Features']['handheld']
-                playercount = s['Features']['playercount']
-                output = Markup("<p>Your perfect game would be" + s['Title'] + "! It has a review score of" + s['Metrics']['Review Score'] + "out of 100. It was released by" + s['Metadata']['Publishers'] + ". Have fun playing!</p>")
+            if x['Features']['Max Players'] == playercount and str(x['Release']['Year']) == year and x['Metrics']['Review Score'] > rating:
+                rating = x['Metrics']['Review Score']
+                year = x['Release']['Year']
+                playercount = x['Features']['Max Players']
+                output = Markup("<p>Your perfect game would be " + x['Title'] + "! It has a review score of " + str(x['Metrics']['Review Score']) + " out of 100. It was released by " + x['Metadata']['Publishers'] + " in " + str(x['Release']['Year']) + ". Have fun playing!</p>")
         return render_template('page1.html', output = output)
     else:
         return render_template('page1.html')
+    
+#@app.route("/page2")
+#def render_page2():
+    #return render_template('page2.html')
+    
 if __name__=="__main__":
     app.run(debug=False)
