a0648d34ff4d1ca547c467c72b157ed8c53c96bd
P1 <you@example.com>
2022-04-13 08:56:55 -0700

    bbbwaaahhhhh

--- webapp.py
+++ webapp.py
@@ -27,9 +27,6 @@ def render_page1():
     else:
         return render_template('page1.html')
     
-#@app.route("/page2")
-#def render_page2():
-    #return render_template('page2.html')
     
 if __name__=="__main__":
     app.run(debug=False)
